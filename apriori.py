51f2aa4e2de63ac2b7da583eb7ac581c80a6b0f1
kenakingkong <kenak@DESKTOP-T22M1KF.localdomain>
2019-04-16 12:22:06 -0700

    added structure

---
+++ apriori.py
@@ -0,0 +1,19 @@
+'''
+    Apriori Algorithm
+'''
+
+import pandas as pd 
+
+def compute_support():
+    pass
+
+def extend_tree():
+    pass
+
+def main():
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ extract_records.py
@@ -0,0 +1,16 @@
+'''
+    program to extract records and export into csv 
+    which is fed into the apriori algorithm
+'''
+
+import pandas as pd 
+
+
+
+def main():
+
+
+
+
+if __name__ == "__main__":
+    main()
