f7b0d3b81ba39d89801c203c24bcbbb9df87dcf2
kenakingkong <kenak@DESKTOP-T22M1KF.localdomain>
2019-04-16 14:24:35 -0700

    more sturcture

--- apriori.py
+++ apriori.py
@@ -1,18 +1,40 @@
 '''
     Apriori Algorithm
 '''
+import pandas as pd
+import sys
 
-import pandas as pd 
+def apriori(D,msup,mconf):
+    #inner functions so that f and c are global
 
-def compute_support():
-    pass
+    # calculates the support for this level of candidates
+    def compute_support(ctree, D):
+        pass
 
-def extend_tree():
+    # adds candidates as leaves from current node
+    def extend_tree(ctree):
+        pass
+
+    # rest of the code here i think
+
+            # create F (result tree)
+            # create C (prefix tree)
+
     pass
 
 def main():
 
+    if (len(sys.argv) != 3):
+        print("Usage: python apriori <file.csv> minsup minconf");
+        sys.exit();
 
+    file = sys.argv[2];
+    minsup = sys.argv[3];
+    minconf = sys.argv[4];
+
+    # turn file into database???
+    # apriori(D,minsup,minconf);
+    apriori(file, minsup, minconf);
 
 
 if __name__ == "__main__":
--- extract_records.py
+++ extract_records.py
@@ -1,16 +1,17 @@
 '''
-    program to extract records and export into csv 
+    program to extract records and export into csv
     which is fed into the apriori algorithm
 '''
 
-import pandas as pd 
+import pandas as pd
 
-
+# function to do the work?
+def work(???):
 
 def main():
-
-
-
+    # import the file
+    # do work
+    # export the file
 
 if __name__ == "__main__":
     main()
