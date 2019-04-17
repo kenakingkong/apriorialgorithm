d30872dbf347c478c10d6705eee1163a8948ee1e
whitneytlarsen <whitneytlarsen@gmail.com>
2019-04-17 14:40:13 -0700

    Added classes and some logic

--- apriori.py
+++ apriori.py
@@ -4,23 +4,49 @@
 import pandas as pd
 import sys
 
-def apriori(D,msup,mconf):
-    #inner functions so that f and c are global
+class Candidate {
+    constructor(){
+        #this.parents =  []; not needed because of level?
+        this.children = [];
+        this.sup = 0;
+        this.conf = 0;
+        this.level = 0;
+    }
+
+    # returns true or false
+    # checks all viable candidates in level before it to see if any are the parent
+    def hasParents():
+        pass;
+}
+
+def apriori(D,I,msup,mconf):
 
     # calculates the support for this level of candidates
-    def compute_support(ctree, D):
+    def compute_support(cTree, D):
+        pass
+
+    # calculates the confidence for this level of candidates
+    def compute_confidence(cTree, D):
         pass
 
     # adds candidates as leaves from current node
-    def extend_tree(ctree):
+    def extend_tree(cTree):
+
         pass
 
-    # rest of the code here i think
+    #starting apriori function
 
-            # create F (result tree)
-            # create C (prefix tree)
+    fTree = Candidate(); # result tree
+    cTree = Candidate(); # prefix tree
 
-    pass
+    # add the first row of items
+    for i in I:
+        child = Candidate();
+        child.level = 1;
+        cTree.children.append(child)
+
+    #
+    while (c)
 
 def main():
 
@@ -32,9 +58,12 @@ def main():
     minsup = sys.argv[3];
     minconf = sys.argv[4];
 
-    # turn file into database???
-    # apriori(D,minsup,minconf);
+    # convert file into database????? set of lists?
+
     apriori(file, minsup, minconf);
+    #association_rules = apriori(file, minsup, minconf);
+    #print association_rules
+
 
 
 if __name__ == "__main__":
